680c8b4dfd1fff0a538410b3cea2f87a8fc287d9
Antoine Prudhomme <antoine@antoines-mbp.home>
2022-11-28 21:46:39 +0100

    able to mint + list URIs of contract tokens

--- hardhat/scripts/deploy.js
+++ hardhat/scripts/deploy.js
@@ -1,12 +1,15 @@
 const hre = require("hardhat");
 
 async function main() {
-  const MaxRabbit = await hre.ethers.getContractFactory("MaxRabbit");
-  const lock = await MaxRabbit.deploy();
+  const maxRabbitFactory = await hre.ethers.getContractFactory("MaxRabbit");
+  const contractOwner = await hre.ethers.getSigner(
+    "0x8626f6940E2eb28930eFb4CeF49B2d1F2C9C1199"
+  );
+  const maxRabbit = await maxRabbitFactory.connect(contractOwner).deploy();
 
-  await lock.deployed();
+  await maxRabbit.deployed();
 
-  console.log(`MaxRabbit deployed to ${lock.address}`);
+  console.log(`MaxRabbit deployed to ${maxRabbit.address}`);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,83 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { ethers } from "ethers";
+import MaxRabbitArtifact from "./MaxRabbit.json";
+
+import "./App.css";
+
+const maxContractAddress = "0x73511669fd4dE447feD18BB79bAFeAC93aB7F31f";
+const ownerAddress = "0x8626f6940e2eb28930efb4cef49b2d1f2c9c1199";
 
 function App() {
+  const [address, setAddress] = useState<string>();
+  const [maxRabbitContract, setMaxRabbitContract] = useState<ethers.Contract>();
+
+  if (typeof window.ethereum !== "undefined") {
+    console.log("MetaMask is installed!");
+  }
+
+  const provider = new ethers.providers.Web3Provider(window.ethereum);
+
+  const loginMetamask = async () => {
+    const addresses = await provider.send("eth_requestAccounts", []);
+    const signer = provider.getSigner();
+    setAddress(addresses[0]);
+
+    setMaxRabbitContract(new ethers.Contract(
+      maxContractAddress,
+      MaxRabbitArtifact.abi,
+      signer
+    ));
+  };
+
+  const handleMintSubmit = async (event: any) => {
+    event.preventDefault();
+    const data = new FormData(event.target);
+    const uri = data.get("uri");
+
+    if (uri && maxRabbitContract) {
+      await maxRabbitContract.safeMint(ownerAddress, uri);
+    }
+  };
+
+  const getAllTokens = async () => {
+    if (maxRabbitContract) {
+      const totalSupply = await maxRabbitContract.totalSupply();
+      const count = parseInt(totalSupply._hex, 16);
+
+      for (let i = 0; i < count; i++) {
+        const token = await maxRabbitContract.tokenByIndex(i);
+        const tokenId = parseInt(token._hex, 16);
+        const tokenURI = await maxRabbitContract.tokenURI(tokenId);
+        console.log("tokenURI", tokenURI);
+      }
+    }
+  };
+
+  const isOwner = address === ownerAddress;
+
+  getAllTokens();
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <div>
+          <h1 className="title">MaxRabbit</h1>
+        </div>
+        <div className="subtitle">NFTs of the cuttest rabbit on earth.</div>
+      </div>
+      <button className="button" id="button-metamask" onClick={loginMetamask}>
+        Login with Metamask
+      </button>
+
+      {isOwner && (
+        <div className="mint-form-container">
+          <div>Mint a new NFT</div>
+          <form className="mint-form" onSubmit={handleMintSubmit}>
+            <label htmlFor="uri">URI</label>
+            <input type="text" name="uri" placeholder="enter metadata uri..." />
+          </form>
+        </div>
+      )}
     </div>
   );
 }
